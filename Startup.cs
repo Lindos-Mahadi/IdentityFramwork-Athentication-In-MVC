d83e1b63df409c2bae87592a3bde2525192225f2
Lindos-Mahadi <lindosmahadi@.com>
2021-07-28 18:53:56 +0600

    Add project files.

---
+++ Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(IdentityFramwork_Athentication_MVC.Startup))]
+namespace IdentityFramwork_Athentication_MVC
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
